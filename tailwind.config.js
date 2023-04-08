51eea2856fe0b9e17504e6b94fe48bb56acf9185
pedrovinhas <pstvideos@gmail.com>
2023-04-08 14:11:14 -0300

    add: modal animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,30 @@ export default {
         85: '85%',
         100: '100%',
       },
+      animation: {
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)'
+      },
+      keyframes: {
+        overlayShow: {
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
+        },
+        contentShow: {
+          from: {
+            opacity: '0',
+            transform: 'translate(-50%, -48%) scale(0.96)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translate(-50%, -50%) scale(1)',
+          },
+        },
+      }
     },
   },
   plugins: [],
