c8e0b67014f110b27611ac9e25583d00ba1b0413
pedrovinhas <pstvideos@gmail.com>
2023-04-08 13:47:42 -0300

    fix: baseURL for api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,5 @@
 import axios from 'axios';
 
-// const token = localStorage.getItem('token');
-
-// console.log(token);
-
-// export const api = axios.create({
-//   baseURL: 'https://api.spotify.com/',
-// });
-
-export const apiAccessToken = axios.create({
-  baseURL: 'https://api.spotify.com/',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    Authorization: 'Basic ' + btoa(import.meta.env.VITE_SPOTIFY_CLIENT_ID + ':'  + import.meta.env.VITE_SPOTIFY_CLIENT_SECRET)
-  },
-  data: 'grant_type=client_credentials',
-  method: 'POST'
+export const api = axios.create({
+  baseURL: 'https://api.spotify.com/v1',
 });
